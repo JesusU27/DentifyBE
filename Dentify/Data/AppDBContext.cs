684418b6c44044c40776fdaf64c820369a63ae33
Jesus <jesus.cont5648@gmail.com>
2024-10-28 18:33:00 -0500

    Initial commit

---
+++ Dentify/Data/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Dentify.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dentify.Data;
+
+public class AppDBContext: DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public AppDBContext(DbContextOptions<AppDBContext> options): base(options){
+    }
+}
---
+++ Dentify/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Dentify.Models;
+
+public class User
+{
+    public int id { get; set; }
+    public string username { get; set; }
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public string email { get; set; }
+    public string phone { get; set; }
+    public DateTime register_date { get; set; }
+    public string company { get; set; }
+    public string password { get; set; }
+}
